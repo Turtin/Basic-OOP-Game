242f071eb3547fb27ccbcc0ad6fc0748976e1c76
Turtin <104549951+Turtin@users.noreply.github.com>
2024-10-17 11:32:16 +1100

    Tweaked the movement

--- Game.py
+++ Game.py
@@ -4,15 +4,18 @@ import keyboard
 import threading
 
 class Entity:
-    speed = 10
+    speed = None
     entity = None
 
-    def __init__(self, x: int, y: int, entity: tk.Frame):
+    def __init__(self, x: int, y: int, speed: int, entity: tk.Frame):
         self.x = x
         self.y = y
         self.entity = entity
+        self.speed = 5
 
     def move(self, x, y):
+        if self.x + x * self.speed < 0 or self.x + x * self.speed > 750 or self.y + y * self.speed < 0 or self.y + y * self.speed > 550:
+            return
         self.x += x * self.speed
         self.y += y * self.speed
         self.render()
@@ -21,23 +24,28 @@ class Entity:
         self.entity.place(x=self.x, y=self.y)
 
 class Player(Entity):
-    speed = 1
+    def __init__(self, x: int, y: int, entity: tk.Frame):
+        super().__init__(x, y, 1, entity)
 
-def inputController(player: Player):
+def inputController(player: Player, window: tk.Tk):
     print("started")
     while True:
         if keyboard.is_pressed('w'):
-            player.move(0, -1)
-            sleep(0.1)
+            player.move(0, -1*player.speed)
+            sleep(0.01667)
         if keyboard.is_pressed('s'):
-            player.move(0, 1)
-            sleep(0.1)
+            player.move(0, 1*player.speed)
+            sleep(0.01667)
         if keyboard.is_pressed('a'):
-            player.move(-1, 0)
-            sleep(0.1)
+            player.move(-1*player.speed, 0)
+            sleep(0.01667)
         if keyboard.is_pressed('d'):
-            player.move(1, 0)
-            sleep(0.1)
+            player.move(1*player.speed, 0)
+            sleep(0.01667)
+        if keyboard.is_pressed('e'):
+            window.configure(bg='blue')
+            sleep(0.01667)
+            window.configure(bg='white')
         print(player.x, player.y)
 
 window = tk.Tk()
@@ -45,8 +53,8 @@ window.title('Game')
 window.geometry('800x600')
 window.resizable(False, False)
 
-player = Entity(0, 0, tk.Frame(window, width=50, height=50, bg='red'))
-input = threading.Thread(target=inputController, args=(player,))
+player = Player(0, 0,tk.Frame(window, width=50, height=50, bg='red'))
+input = threading.Thread(target=inputController, args=(player, window))
 input.start()
 
 window.mainloop()
