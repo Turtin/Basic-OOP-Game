1bb65b7dc44305b3eb110265e83207f87c2a6880
Turtin <104549951+Turtin@users.noreply.github.com>
2024-10-20 22:25:08 +1100

    Made so that the game doesn't do anything when not in focus, Tried to make some colliders, unsure currently why they don't work currently. Also, there's the diagonal movement bug, but not that significant.

--- Game.py
+++ Game.py
@@ -10,12 +10,17 @@ class Entity:
     name = None
     entity = None
     coordinates = (0, 0)
+    colliders = []
+    size = 0
 
-    def __init__(self, x: int, y: int, speed: int, name: str,entity: tk.Frame): # Entity constructor
+    def __init__(self, x: int, y: int, speed: int, size: int, name: str,entity: tk.Frame): # Entity constructor
         self.coordinates = (x, y)
         self.entity = entity
         self.speed = speed
         self.name = name
+        self.size = size
+
+        print(f'{datetime.datetime.now().strftime('%H:%M:%S')}: Entity "{self.name}" created at {self.coordinates[0]}, {self.coordinates[1]} with a speed of {self.speed} and a size of {self.entity.winfo_width()}x{self.entity.winfo_height()}') # Debugging
 
     def move(self, x, y): # Move function
         # Check if the entity is out of bounds
@@ -24,12 +29,19 @@ class Entity:
         # Move the entity
         self.coordinates = (self.coordinates[0] + x * self.speed, self.coordinates[1] + y * self.speed)
 
+        self.colliders = [
+            (self.coordinates[0], self.coordinates[1]),
+            (self.coordinates[0], self.coordinates[1] + self.size),
+            (self.coordinates[0] + self.size, self.coordinates[1]),
+            (self.coordinates[0] + self.size, self.coordinates[1] + self.size)
+        ]
+
     def render(self): # Renders the entity
         self.entity.place(x=self.coordinates[0], y=self.coordinates[1])
 
 class Player(Entity): # Player class
-    def __init__(self, x: int, y: int, name: str, entity: tk.Frame): # Player constructor
-        super().__init__(x, y, 2.5, name, entity)
+    def __init__(self): # Player constructor
+        super().__init__(0, 0, 2, 50,"Player", tk.Frame(window, width=50, height=50, bg='red'))
 
     def pulse(self, window: tk.Tk):
         self.entity.configure(width=60, height=60)
@@ -41,9 +53,35 @@ class Player(Entity): # Player class
         window.configure(bg='white')
         sleep(0.1)
 
+    def deathDetection(self):
+        for entity in Entities:
+            if entity.name == "Enemy":
+                if (entity.colliders[0][0] < self.colliders[4][0] and entity.colliders[0][1] < self.colliders[4][1]) or (entity.colliders[1][0] < self.colliders[4][0] and entity.colliders[1][1] > self.colliders[4][1]) or (entity.colliders[2][0] > self.colliders[4][0] and entity.colliders[2][1] < self.colliders[4][1]) or (entity.colliders[3][0] > self.colliders[4][0] and entity.colliders[3][1] > self.colliders[4][1]):
+                    print('Death')
+                    window.destroy()
+
+
+class Enemy(Entity): # Enemy class
+    def __init__(self): # Enemy constructor
+        super().__init__(700, 500, 1, 20, "Enemy", tk.Frame(window, width=20, height=20, bg='green'))
+
+    def trackPlayer(self, player: Player):
+        if player.coordinates[0] + 15 > self.coordinates[0]:
+            self.move(1, 0)
+        if player.coordinates[0] + 15 < self.coordinates[0]:
+            self.move(-1, 0)
+        if player.coordinates[1] + 15 > self.coordinates[1]:
+            self.move(0, 1)
+        if player.coordinates[1] + 15 < self.coordinates[1]:
+            self.move(0, -1)
+
+
 def inputController(player: Player, window: tk.Tk, coords: tk.Label): # Input controller function to detect key presses
     print(f"{datetime.datetime.now().strftime('%H:%M:%S')}: Starting input controller") # Debugging
     while True: # Loop to detect key presses constantly
+        if window.focus_get() == None:
+            continue
+
         if keyboard.is_pressed('w'):
             player.move(0, -1)
         if keyboard.is_pressed('s'):
@@ -62,10 +100,19 @@ def inputController(player: Player, window: tk.Tk, coords: tk.Label): # Input co
 
 def ticker(coords: tk.Label, player: Player): # Ticker function to update the game
     print(f"{datetime.datetime.now().strftime('%H:%M:%S')}: Starting ticker controller")  # Debugging
+    global Entities
     while True: # Loop to update the game constantly
+        if window.focus_get() == None: # Pauses the game when out of focus
+            continue
+
         coords.configure(text=f'X: {player.coordinates[0]} Y: {player.coordinates[1]}')
-        player.render()
+        for entity in Entities:
+            entity.render()
+            if entity.name == "Enemy":
+                entity.trackPlayer(player)
         sleep(0.01667)
+
+Entities = [] # List of entities
 
 # Window
 window = tk.Tk()
@@ -75,7 +122,10 @@ window.resizable(False, False)
 lbl = tk.Label(window, text='', font=('Arial Bold', 50))
 
 # Entity starter
-player = Player(0, 0, "Player",tk.Frame(window, width=50, height=50, bg='red'))
+player = Player()
+enemy = Enemy()
+Entities.append(enemy)
+Entities.append(player)
 
 # Input controller
 input = threading.Thread(target=inputController, args=(player, window, lbl))
